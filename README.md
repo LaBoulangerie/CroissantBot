cf1fd07fc10146a511b7910b1a5e55f76178dfb1
PainOchoco <matteo.planchet@icloud.com>
2023-02-07 19:50:40 +0100

    📦 Added global deploying

--- README.md
+++ README.md
@@ -5,4 +5,10 @@ Discord Bot for La Boulangerie using Discord.js v14.
 ## ⛏️ Building
 
 To build the bot just run `npm run build` and make sure to create the environment variable file named `.env`.
-Here are the environment variables to set: `DISCORD_TOKEN, CLIENT_ID, GUILD_ID, MAIN_COLOR, API_BASE_URL, WIKI_BASE_URL, WIKI_EMOJI, BAGUETTE_EMOJI, MONEY_BAGUETTE_EMOJI, MONEY_CROISSANT_EMOJI, MONEY_BRIOCHE_EMOJI, WELCOME_CHANNEL_ID, LOG_CHANNEL_ID, VERIF_CHANNEL_ID, NATIONS_CATEGORY_ID, MEMBER_ROLE_ID, FILE_EXTENSION`
+Here are the environment variables to set: `DISCORD_TOKEN, CLIENT_ID, GUILD_ID, MAIN_COLOR, API_BASE_URL, WIKI_BASE_URL, WIKI_EMOJI, BAGUETTE_EMOJI, MONEY_BAGUETTE_EMOJI, MONEY_CROISSANT_EMOJI, MONEY_BRIOCHE_EMOJI, WELCOME_CHANNEL_ID, LOG_CHANNEL_ID, VERIF_CHANNEL_ID, NATIONS_CATEGORY_ID, MEMBER_ROLE_ID, FILE_EXTENSION`.
+
+## 📦 Deploy commands
+
+Two options : global or guild, depends on what you're doing. Guild-based deployment of commands is best suited for development and testing in your own personal server. Once you're satisfied that it's ready, deploy the command globally to publish it to all guilds that your bot is in.
+
+Then run `node deploy-commands.ts <global|guild>`.
--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -2,13 +2,14 @@ import { REST, Routes } from "discord.js";
 import * as fs from "fs";
 import config from "./config";
 const commands = [];
+const mode = process.argv[2];
 
 const commandFiles = fs
-    .readdirSync("./src/commands")
+    .readdirSync("./commands")
     .filter((file) => file.endsWith(config.fileExtension));
 
 for (const file of commandFiles) {
-    const command = require(`./src/commands/${file}`).default.data;
+    const command = require(`./commands/${file}`).default.data;
     commands.push(command);
 }
 
@@ -16,18 +17,29 @@ const rest = new REST({ version: "10" }).setToken(config.discordToken);
 
 (async () => {
     try {
-        console.log(
-            `Started refreshing ${commands.length} application (/) commands.`
-        );
+        console.log(`Started refreshing ${commands.length} application commands.`);
 
-        const data = (await rest.put(
-            Routes.applicationGuildCommands(config.clientID, config.guildID),
-            { body: commands }
-        )) as Array<any>;
+        let data: Array<any>;
+        switch (mode) {
+            case "global":
+                data = (await rest.put(Routes.applicationCommands(config.clientID), {
+                    body: commands,
+                })) as Array<any>;
+                break;
 
-        console.log(
-            `Successfully reloaded ${data.length} application (/) commands.`
-        );
+            case "guild":
+                data = (await rest.put(
+                    Routes.applicationGuildCommands(config.clientID, config.guildID),
+                    { body: commands }
+                )) as Array<any>;
+                break;
+
+            default:
+                console.log("Mode not found, use 'global' or 'guild'");
+                return;
+        }
+
+        console.log(`Successfully reloaded ${data.length} ${mode} application commands.`);
     } catch (error) {
         console.error(error);
     }
